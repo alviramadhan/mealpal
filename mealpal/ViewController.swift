a8ba2efe9439e87a5f4f9d2aada8050659332387
alviramadhan <alviramadhan77@gmail.com>
2025-03-14 17:34:34 +1100

    Initial Commit

---
+++ mealpal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mealpal
+//
+//  Created by Alvi Ramadhan on 14/3/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
