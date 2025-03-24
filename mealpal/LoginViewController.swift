1f1e72800feaea5851a9ebb31572186927d638cf
alviramadhan <alviramadhan77@gmail.com>
2025-03-24 21:28:09 +1100

    colors

--- mealpal/LoginViewController.swift
+++ mealpal/LoginViewController.swift
@@ -7,14 +7,63 @@
 
 import UIKit
 
+import UIKit
+
 class LoginViewController: UIViewController {
 
+    // MARK: - UI Components
+    private let emailField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.borderStyle = .roundedRect
+        tf.autocapitalizationType = .none
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
 
+    private let passwordField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.borderStyle = .roundedRect
+        tf.isSecureTextEntry = true
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+
+    private let loginButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Log In", for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setupLayout()
     }
+
+    // MARK: - Layout Constraints
+    private func setupLayout() {
+        view.addSubview(emailField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+
+        NSLayoutConstraint.activate([
+            emailField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            emailField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            emailField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+
+            passwordField.topAnchor.constraint(equalTo: emailField.bottomAnchor, constant: 16),
+            passwordField.leadingAnchor.constraint(equalTo: emailField.leadingAnchor),
+            passwordField.trailingAnchor.constraint(equalTo: emailField.trailingAnchor),
+
+            loginButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 32),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+}
     
 
     /*
@@ -27,4 +76,4 @@ class LoginViewController: UIViewController {
     }
     */
 
-}
+
