63440189604fd2d2c4e485950f3ba4821fbefb8e
alviramadhan <alviramadhan77@gmail.com>
2025-04-04 13:02:15 +1100

    update sign up,splash,login UI

--- mealpal/LoginViewController.swift
+++ mealpal/LoginViewController.swift
@@ -7,73 +7,29 @@
 
 import UIKit
 
-import UIKit
-
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginPasswordTextField: UITextField!
+    @IBOutlet weak var loginEmailTextField: UITextField!
+    @IBOutlet weak var loginLoginbutton: UIButton!
 
-    // MARK: - UI Components
-    private let emailField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Email"
-        tf.borderStyle = .roundedRect
-        tf.autocapitalizationType = .none
-        tf.translatesAutoresizingMaskIntoConstraints = false
-        return tf
-    }()
-
-    private let passwordField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Password"
-        tf.borderStyle = .roundedRect
-        tf.isSecureTextEntry = true
-        tf.translatesAutoresizingMaskIntoConstraints = false
-        return tf
-    }()
-
-    private let loginButton: UIButton = {
-        let btn = UIButton(type: .system)
-        btn.setTitle("Log In", for: .normal)
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        return btn
-    }()
-
-    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        setupLayout()
+        
+        let eyeButton = UIButton(type: .custom)
+        eyeButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        eyeButton.tintColor = .gray
+        eyeButton.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        eyeButton.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
+        
+        loginPasswordTextField.rightView = eyeButton
+        loginPasswordTextField.rightViewMode = .always
+        loginPasswordTextField.isSecureTextEntry = true
     }
 
-    // MARK: - Layout Constraints
-    private func setupLayout() {
-        view.addSubview(emailField)
-        view.addSubview(passwordField)
-        view.addSubview(loginButton)
-
-        NSLayoutConstraint.activate([
-            emailField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
-            emailField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-            emailField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-
-            passwordField.topAnchor.constraint(equalTo: emailField.bottomAnchor, constant: 16),
-            passwordField.leadingAnchor.constraint(equalTo: emailField.leadingAnchor),
-            passwordField.trailingAnchor.constraint(equalTo: emailField.trailingAnchor),
-
-            loginButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 32),
-            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
+    @objc func togglePasswordVisibility(_ sender: UIButton) {
+        loginPasswordTextField.isSecureTextEntry.toggle()
+        let imageName = loginPasswordTextField.isSecureTextEntry ? "eye.slash" : "eye"
+        sender.setImage(UIImage(systemName: imageName), for: .normal)
     }
 }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
--- mealpal/SignUpViewController.swift
+++ mealpal/SignUpViewController.swift
@@ -8,14 +8,50 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var signupUsernameTextField: UITextField!
+    @IBOutlet weak var signupConfirmPasswordTextFIeld: UITextField!
+    @IBOutlet weak var signupPasswordTextField: UITextField!
+    @IBOutlet weak var signupEmailTextField: UITextField!
     override func viewDidLoad() {
-        super.viewDidLoad()
+        
+        let eyeButton = UIButton(type: .custom)
+        eyeButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        eyeButton.tintColor = .gray
+        eyeButton.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        eyeButton.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
+        
+        let eyeButton2 = UIButton(type: .custom)
+        eyeButton2.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        eyeButton2.tintColor = .gray
+        eyeButton2.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        eyeButton2.addTarget(self, action: #selector(togglePasswordVisibility2), for: .touchUpInside)
+        
+        signupPasswordTextField.rightView = eyeButton
+        signupPasswordTextField.rightViewMode = .always
+        signupPasswordTextField.isSecureTextEntry = true
+        
+        signupConfirmPasswordTextFIeld.rightView = eyeButton2
+        signupConfirmPasswordTextFIeld.rightViewMode = .always
+        signupConfirmPasswordTextFIeld.isSecureTextEntry = true
+    }
 
-        // Do any additional setup after loading the view.
+    @objc func togglePasswordVisibility(_ sender: UIButton) {
+        signupPasswordTextField.isSecureTextEntry.toggle()
+        let imageName = signupPasswordTextField.isSecureTextEntry ? "eye.slash" : "eye"
+        sender.setImage(UIImage(systemName: imageName), for: .normal)
     }
     
+    @objc func togglePasswordVisibility2(_ sender: UIButton) {
+        signupConfirmPasswordTextFIeld.isSecureTextEntry.toggle()
+        let imageName = signupConfirmPasswordTextFIeld.isSecureTextEntry ? "eye.slash" : "eye"
+        sender.setImage(UIImage(systemName: imageName), for: .normal)
+    }
+}
 
+
+ 
     /*
     // MARK: - Navigation
 
@@ -26,4 +62,4 @@ class SignUpViewController: UIViewController {
     }
     */
 
-}
+
